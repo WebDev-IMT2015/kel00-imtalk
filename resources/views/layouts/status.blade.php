e4e86d4e107bddc642954b0b8261754d858f2a9d
Yefta Sutanto <yeftasutanto@gmail.com>
2017-06-18 22:11:50 +0700

    update status

--- resources/views/layouts/status.blade.php
+++ resources/views/layouts/status.blade.php
@@ -6,8 +6,7 @@
                 <li>Posts: 123</li>
                 <li>Likes Get: 123</li>
                 <li>Likes Given: 123</li>
-                <li><strong>Registered</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->created_at)) }}</li>
-                <li><strong>Last Login</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->last_login)) }}</li>
+                <li><strong>Registered</strong> {{ date('H:i:s d/m/Y', strtotime(Auth::user()->created_at)) }}</li>
                 <li><strong>Last Login</strong> {{ date('H:i:s d/m/Y', strtotime(Auth::user()->last_login)) }}</li>
             </ul>
         </div>
