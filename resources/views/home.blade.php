95125cc2d1ea89cd2824a3ad6621b5f8d5ff3728
Yefta Sutanto <yeftasutanto@gmail.com>
2017-06-18 21:55:36 +0700

    separate side nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ Auth::user()->name }}</div>
-                <div class="panel-body">
-                    <ul style="padding-left: 20px">
-                        <li>Posts: 123</li>
-                        <li>Likes Get: 123</li>
-                        <li>Likes Given: 123</li>
-                        <li><strong>Registered</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->created_at)) }}</li>
-                        <li><strong>Last Login</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->last_login)) }}</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        @include('layouts.status')
         <div class="col-md-9">
             <div class="panel panel-default">
                 <div class="panel-heading" style="padding-bottom: 20px">Threads <span class="pull-right"><a href="{{ route('post.create') }}" class="btn btn-sm btn-success"><i class="fa fa-plus"></i>&nbsp;&nbsp;New Thread</a></span></div>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,20 +9,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ Auth::user()->name }}</div>
-                <div class="panel-body">
-                    <ul style="padding-left: 20px">
-                        <li>Posts: 123</li>
-                        <li>Likes Get: 123</li>
-                        <li>Likes Given: 123</li>
-                        <li><strong>Registered</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->created_at)) }}</li>
-                        <li><strong>Last Login</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->last_login)) }}</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        @include('layouts.status')
         <div class="col-md-9">
             <div class="panel panel-default">
                 <div class="panel-heading">Create New Thread</div>
