43a1100256d9b1633ab83524ebcf1173e6e971cf
Yefta Sutanto <yeftasutanto@gmail.com>
2017-06-18 22:11:44 +0700

    update status

--- resources/views/layouts/status.blade.php
+++ resources/views/layouts/status.blade.php
@@ -8,6 +8,7 @@
                 <li>Likes Given: 123</li>
                 <li><strong>Registered</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->created_at)) }}</li>
                 <li><strong>Last Login</strong> {{ date('H:i:s d/m/y', strtotime(Auth::user()->last_login)) }}</li>
+                <li><strong>Last Login</strong> {{ date('H:i:s d/m/Y', strtotime(Auth::user()->last_login)) }}</li>
             </ul>
         </div>
     </div>
