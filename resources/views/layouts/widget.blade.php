ae624bd76a4369e90d7754128956556f96e4676f
Yefta Sutanto <yeftasutanto@gmail.com>
2017-06-18 23:46:53 +0700

    user post count

--- resources/views/layouts/widget.blade.php
+++ resources/views/layouts/widget.blade.php
@@ -1,9 +1,9 @@
 <div class="col-md-3">
     <div class="panel panel-default">
-        <div class="panel-heading">{{ Auth::user()->name }}</div>
+        <div class="panel-heading"><strong>{{ Auth::user()->name }}</strong></div>
         <div class="panel-body">
             <ul style="padding-left: 20px">
-                <li>Posts: 123</li>
+                <li>Posts: {{ App\Models\Post::where('id_user', Auth::user()->id_user)->count() }}</li>
                 <li>Likes Get: 123</li>
                 <li>Likes Given: 123</li>
                 <li><strong>Registered</strong> {{ date('H:i:s d/m/Y', strtotime(Auth::user()->created_at)) }}</li>
